19a4a48182e542d4bb6b23e468ed70eac33f6b74
oleksandr <oleksandrmoroz2003@gmail.com>
2024-04-24 19:09:29 +0300

    make models for regions and alerts

--- alerts/models.py
+++ alerts/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+
+class Region(models.Model):
+    regionId = models.AutoField(primary_key=True)
+    regionName = models.TextField()
+    regionType = models.TextField()
+
+    def __str__(self):
+        return self.regionName
+
+
+class Alert(models.Model):
+    alertId = models.AutoField(primary_key=True)
+    region = models.ForeignKey(Region, on_delete=models.CASCADE, db_column='regionId')
+    startTime = models.DateTimeField()
+    endTime = models.DateTimeField()
+    duration = models.DurationField()
+    alertType = models.TextField()
+    isContinue = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.alertType} from {self.startTime} to {self.endTime}"
