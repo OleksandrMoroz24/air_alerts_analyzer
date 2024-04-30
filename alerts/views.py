27d65e5245e8e371d9808bdc1a528ed7ff69fb64
oleksandr <oleksandrmoroz2003@gmail.com>
2024-04-30 20:14:04 +0300

    fixes

--- alerts/views.py
+++ alerts/views.py
@@ -6,6 +6,12 @@ from django.db.models.functions import ExtractWeekDay, ExtractHour
 from .models import Alerts, Regions
 from datetime import datetime
 
+def format_timedelta(td):
+    total_seconds = int(td.total_seconds())
+    hours = total_seconds // 3600
+    minutes = (total_seconds % 3600) // 60
+    return f"{hours} hours, {minutes} minutes" if hours else f"{minutes} minutes"
+
 def region_alerts_analytics(request):
     regions = Regions.objects.all()
     selected_region = request.POST.get('regionid')
@@ -35,18 +41,17 @@ def region_alerts_analytics(request):
 
         context['alerts'] = alerts
         context['count'] = alerts.count()
-        context['average_duration'] = alerts.aggregate(Avg('duration'))['duration__avg']
-        context['max_duration'] = alerts.aggregate(Max('duration'))['duration__max']
+        context['average_duration'] = format_timedelta(alerts.aggregate(Avg('duration'))['duration__avg'])
+        context['max_duration'] = format_timedelta(alerts.aggregate(Max('duration'))['duration__max'])
         context['chart_type'] = chart_type
 
-        if chart_type == 'weekday':
-            weekday_counts = alerts.annotate(weekday=ExtractWeekDay('starttime')).values('weekday').annotate(
-                count=Count('alertid')).order_by('weekday')
-            context['weekday_counts'] = {day['weekday']: day['count'] for day in weekday_counts}
-        if chart_type == 'hour':
-            hour_counts_query = alerts.annotate(hour=ExtractHour('starttime')).values('hour').annotate(
-                count=Count('alertid')).order_by('hour')
-            hour_counts = {hour['hour']: hour['count'] for hour in hour_counts_query}
-            context['hour_counts_list'] = [hour_counts.get(hour, 0) for hour in range(24)]
+        weekday_counts = alerts.annotate(weekday=ExtractWeekDay('starttime')).values('weekday').annotate(
+            count=Count('alertid')).order_by('weekday')
+        context['weekday_counts'] = {day['weekday']: day['count'] for day in weekday_counts}
+
+        hour_counts_query = alerts.annotate(hour=ExtractHour('starttime')).values('hour').annotate(
+            count=Count('alertid')).order_by('hour')
+        hour_counts = {hour['hour']: hour['count'] for hour in hour_counts_query}
+        context['hour_counts_list'] = [hour_counts.get(hour, 0) for hour in range(24)]
 
     return render(request, 'alerts_analytics.html', context)
