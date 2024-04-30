99e78b4be7f6941d703b126fb4e9b2003b09dd7d
oleksandr <oleksandrmoroz2003@gmail.com>
2024-04-30 15:19:47 +0300

    add charts

--- alerts/views.py
+++ alerts/views.py
@@ -1,45 +1,52 @@
 # views.py
+
 from django.shortcuts import render
 from django.db.models import Count, Avg, Max
+from django.db.models.functions import ExtractWeekDay, ExtractHour
 from .models import Alerts, Regions
 from datetime import datetime
 
-
 def region_alerts_analytics(request):
+    regions = Regions.objects.all()
+    selected_region = request.POST.get('regionid')
+    start_date = request.POST.get('startdate') or datetime.now().strftime('%Y-%m-%d')  # default to today if not provided
+    end_date = request.POST.get('enddate') or datetime.now().strftime('%Y-%m-%d')  # default to today if not provided
+    chart_type = request.POST.get('charttype', 'hour')  # Default to 'hour'
+
     context = {
-        'regions': Regions.objects.all(),
-        'alerts': None,
-        'count': None,
-        'average_duration': None,
-        'max_duration': None
+        'regions': regions,
+        'selected_region': selected_region,
+        'start_date': start_date,
+        'end_date': end_date,
+        'chart_type': chart_type,
+        # Initialize other context variables here...
     }
 
     if request.method == 'POST':
-        region_id = request.POST.get('regionid')
-        start_date_str = request.POST.get('startdate')
-        end_date_str = request.POST.get('enddate')
-
-        # Convert user input into datetime objects assuming input format is %Y-%m-%d (HTML5 date input format)
-        start_datetime = datetime.strptime(start_date_str, '%Y-%m-%d')
-        end_datetime = datetime.strptime(end_date_str, '%Y-%m-%d')
-
-        # Adjust the end time to include the entire day
+        start_datetime = datetime.strptime(start_date, '%Y-%m-%d')
+        end_datetime = datetime.strptime(end_date, '%Y-%m-%d')
         end_datetime = end_datetime.replace(hour=23, minute=59, second=59)
 
-        # Filter alerts based on the selected region and time period
         alerts = Alerts.objects.filter(
-            regionid_id=region_id,
+            regionid_id=selected_region,
             starttime__gte=start_datetime,
             endtime__lte=end_datetime
-        )
+        ).order_by('-starttime')
 
         context['alerts'] = alerts
-
-        # Calculate count, average duration, and maximum duration
         context['count'] = alerts.count()
-        avg_duration = alerts.aggregate(Avg('duration'))['duration__avg']
-        max_duration = alerts.aggregate(Max('duration'))['duration__max']
-        context['average_duration'] = avg_duration and avg_duration.total_seconds() // 60  # Convert to minutes
-        context['max_duration'] = max_duration and max_duration.total_seconds() // 60  # Convert to minutes
+        context['average_duration'] = alerts.aggregate(Avg('duration'))['duration__avg']
+        context['max_duration'] = alerts.aggregate(Max('duration'))['duration__max']
+        context['chart_type'] = chart_type
+
+        if chart_type == 'weekday':
+            weekday_counts = alerts.annotate(weekday=ExtractWeekDay('starttime')).values('weekday').annotate(
+                count=Count('alertid')).order_by('weekday')
+            context['weekday_counts'] = {day['weekday']: day['count'] for day in weekday_counts}
+        if chart_type == 'hour':
+            hour_counts_query = alerts.annotate(hour=ExtractHour('starttime')).values('hour').annotate(
+                count=Count('alertid')).order_by('hour')
+            hour_counts = {hour['hour']: hour['count'] for hour in hour_counts_query}
+            context['hour_counts_list'] = [hour_counts.get(hour, 0) for hour in range(24)]
 
     return render(request, 'alerts_analytics.html', context)
